b9c517b79d9a83c847dcf8c24da5564087c94efb
Kallol Dey <kalloldey067@gmail.com>
2026-05-19 09:49:37 +0600

    issue fixed

--- src/app/ideas/page.jsx
+++ src/app/ideas/page.jsx
@@ -1,15 +1,30 @@
 import IdeaCard from "@/components/IdeaCard";
 
-
 const IdeasPage = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/idea`);
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/idea`,
+        {
+            cache:"no-store"
+        }
+    );
+
     const ideas = await res.json();
+
     return (
+
         <div className="grid grid-cols-4 gap-4 mt-10">
+
             {
-                ideas.map((idea) => <IdeaCard key={idea._id} idea={idea} />)
+                ideas.map((idea)=>(
+                    <IdeaCard
+                        key={idea._id}
+                        idea={idea}
+                    />
+                ))
             }
+
         </div>
+
     );
 };
 
